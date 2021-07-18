4a53ecfe200b910db445641e308d08beeb401d8c
TJUMMG <69636939+TJUMMG@users.noreply.github.com>
2021-07-18 12:03:28 +0800

    Add files via upload

---
+++ RMF_4bit/RMF_test_4_16/data/ImageData.py
@@ -0,0 +1,79 @@
+import os
+
+from data import common
+import cv2
+import torch.utils.data as data
+
+class ImageData(data.Dataset):
+    def __init__(self, args, name='', train=True, benchmark=False):
+        self.args = args
+        self.name = name
+        self.train = train
+        self.split = 'train' if train else 'test'
+        self.benchmark = benchmark
+        self.scale = args.scale
+        self.testbin = args.testbin
+        self._set_filesystem(args.dir_data)
+
+
+        print('initial image data now!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print('benchmark',self.benchmark)
+        if self.benchmark:
+            print('BenchmarkScaning')
+            self.images_tar, self.images_input = self._scan()
+            print('Scan finished!')
+        elif args.ext == 'img':
+            self.images_tar, self.images_input = self._scan()
+        else:
+            print('Please define data type')
+
+    def _scan(self):
+        raise NotImplementedError
+
+    def _set_filesystem(self, dir_data):
+        raise NotImplementedError
+
+    def _name_tarbin(self):
+        raise NotImplementedError
+
+    def _name_inputbin(self, scale):
+        raise NotImplementedError
+
+
+
+
+    def __getitem__(self, idx):
+
+        img_input, img_tar = self._load_file(idx)
+        img_input, img_tar = common.set_channel([img_input, img_tar], self.args.n_colors)
+        img_input, img_tar = self._get_patch(img_input, img_tar)
+        input_tensor, tar_tensor = common.np2Tensor([img_input, img_tar], self.args.rgb_range)
+        return input_tensor, tar_tensor
+
+    def __len__(self):
+        return len(self.images_tar)
+
+    def _get_index(self, idx):
+        return idx
+
+    def _load_file(self, idx):
+
+        idx = self._get_index(idx)
+        if self.benchmark and self.testbin:
+            img_input = self.images_input[idx]
+            img_tar = self.images_tar[idx]
+        elif self.benchmark:
+            img_input = cv2.imread(self.images_input[idx], 3)
+            img_tar = cv2.imread(self.images_tar[idx], 3)
+        elif self.args.ext == 'img':
+            img_input = cv2.imread(self.images_input[idx], 3)
+            img_tar = cv2.imread(self.images_tar[idx], 3)
+        return img_input, img_tar
+
+    def _get_patch(self, img_input, img_tar):
+        scale = self.scale
+        img_input, img_tar = common.get_patch_test(img_input, img_tar, scale)
+        return img_input, img_tar
+
+
+
---
+++ RMF_4bit/RMF_test_4_16/data/benchmarktest.py
@@ -0,0 +1,33 @@
+import os
+from data import ImageData
+
+
+
+class BenchmarkTest(ImageData.ImageData):
+    def __init__(self, args, name = '', train=True):
+        super(BenchmarkTest, self).__init__(args, name = name, train=train, benchmark=True)
+
+    def _scan(self):
+        list_tar = []
+        list_input = []
+
+        # UST-HK
+        idx_begin = 0
+        idx_end = 40
+
+
+
+        # UST-HK
+        for i in range(idx_begin + 1, idx_end + 1):
+            filename = '{:0>4}'.format(i)
+            list_tar.append(os.path.join(self.apath_tar, filename + self.ext))
+            list_input.append(os.path.join(self.apath_in, filename + self.ext))
+
+        return list_tar, list_input
+
+
+    def _set_filesystem(self, dir_data):
+        # UST-HK
+        self.apath_tar = os.path.join(dir_data, 'HK', 'Source1200')
+        self.apath_in = os.path.join(dir_data, 'HK', 'Source1200_4b')
+        self.ext = '.png'
