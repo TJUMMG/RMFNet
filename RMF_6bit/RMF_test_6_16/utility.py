b0236b9a98b6de1b075f18f31a25a3bf5adb32dd
TJUMMG <69636939+TJUMMG@users.noreply.github.com>
2021-07-18 16:20:55 +0800

    Add files via upload

---
+++ RMF_6bit/RMF_test_6_16/utility.py
@@ -0,0 +1,168 @@
+import os
+import math
+import time
+import datetime
+from multiprocessing import Process
+from multiprocessing import Queue
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import torch
+import cv2
+
+class timer():
+    def __init__(self):
+        self.acc = 0
+        self.tic()
+
+    def tic(self):
+        self.t0 = time.clock()
+
+    def toc(self):
+        return time.clock() - self.t0
+
+    def hold(self):
+        self.acc += self.toc()
+
+    def release(self):
+        ret = self.acc
+        self.acc = 0
+
+        return ret
+
+    def reset(self):
+        self.acc = 0
+class checkpoint():
+    def __init__(self, args):
+        self.args = args
+        self.ok = True
+        self.log = torch.Tensor()
+        now = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+
+        if not args.load:
+            if not args.save:
+                args.save = now
+            self.dir = os.path.join(args.save)
+        else:
+            self.dir = os.path.join(args.load)
+
+
+        if args.reset:
+            os.system('rm -rf ' + self.dir)
+            args.load = ''
+
+        os.makedirs(self.dir, exist_ok=True)
+        os.makedirs(self.get_path('model'), exist_ok=True)
+        os.makedirs(self.get_path('results-{}'.format(args.data_test)), exist_ok=True)
+        open_type = 'a' if os.path.exists(self.get_path('log.txt'))else 'w'
+        self.log_file = open(self.get_path('log.txt'), open_type)
+        with open(self.get_path('config.txt'), open_type) as f:
+            f.write(now + '\n\n')
+            for arg in vars(args):
+                f.write('{}: {}\n'.format(arg, getattr(args, arg)))
+            f.write('\n')
+
+        self.n_processes = 8
+
+    def get_path(self, *subdir):
+        return os.path.join(self.dir, *subdir)
+
+    def save(self, trainer, epoch, is_best=False):
+        trainer.model.save(self.get_path('model'), epoch, is_best=is_best)
+        trainer.loss.save(self.dir)
+        trainer.loss.plot_loss(self.dir, epoch)
+
+        self.plot_psnr(epoch)
+        trainer.optimizer.save(self.dir)
+        torch.save(self.log, self.get_path('psnr_log.pt'))
+
+    def add_log(self, log):
+        self.log = torch.cat([self.log, log])
+
+    def write_log(self, log, refresh=False):
+        print(log)
+        self.log_file.write(log + '\n')
+        if refresh:
+            self.log_file.close()
+            self.log_file = open(self.get_path('log.txt'), 'a')
+
+    def done(self):
+        self.log_file.close()
+
+    def plot_psnr(self, epoch):
+        axis = np.linspace(1, epoch, epoch)
+        label = 'SR on {}'.format(self.args.data_test)
+        fig = plt.figure()
+        plt.title(label)
+        plt.plot(
+                    axis,
+                    self.log[:].numpy()
+                )
+        plt.legend()
+        plt.xlabel('Epochs')
+        plt.ylabel('PSNR')
+        plt.grid(True)
+        plt.savefig(self.get_path('test_{}.pdf'.format(self.args.data_test)))
+        plt.close(fig)
+
+    def begin_background(self):
+        self.queue = Queue()
+
+        def bg_target(queue):
+            while True:
+                if not queue.empty():
+                    filename, tensor = queue.get()
+                    if filename is None: break
+                    cv2.imwrite(filename, np.array(tensor.numpy(), dtype=np.uint16))
+        
+        self.process = [
+            Process(target=bg_target, args=(self.queue,)) \
+            for _ in range(self.n_processes)
+        ]
+        
+        for p in self.process: p.start()
+
+    def end_background(self):
+        for _ in range(self.n_processes): self.queue.put((None, None))
+        while not self.queue.empty(): time.sleep(1)
+        for p in self.process: p.join()
+
+    def save_results(self, dataset, filename, save_list):
+        if self.args.save_results:
+            filename = self.get_path(
+                'results-{}'.format(dataset.dataset.name),
+                '{}_'.format(filename)
+            )
+
+            postfix = ('HBD_RMF_6_16', 'HBD')
+            for v, p in zip(save_list, postfix):
+                normalized = v[0]
+                tensor_cpu = normalized.permute(1, 2, 0).cpu()
+
+                self.queue.put(('{}{}.png'.format(filename, p), tensor_cpu))
+
+
+
+
+def quantize(img, rgb_range):
+    pixel_range = 65535 / rgb_range         ###
+    return img.mul(pixel_range).clamp(0, 65535).round().div(pixel_range)    ###
+
+
+
+
+def calc_psnr(sr, hr, scale, rgb_range, benchmark=False):
+    diff = (sr - hr) / rgb_range
+    shave = 0
+    mse = diff.pow(2).mean()
+    return -10 * math.log10(mse)
+
+
+
+
+
+
